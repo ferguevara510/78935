dc66ef2ae61e860fd80c99f50e40028f63405d2c
Fer Guevara <kafera.g10@gmail.com>
2022-03-10 20:11:09 -0600

    Ejercicio Axis2
    Ejercicio de Axis2 de Calculadora en Docker

---
+++ asix2-calculadora/calculadora.java
@@ -0,0 +1,35 @@
+https://dlcdn.apache.org/axis/axis2/java/core/1.8.0/axis2-1.8.0-bin.zip -o /opt/axis2.zip
+
+public class calculadora {
+
+ public int suma (int a, int b){
+
+  return a+b;
+
+ }
+
+ 
+
+ public int resta (int a, int b){
+
+  return a-b;
+
+ }
+
+ 
+
+ public int multiplica (int a, int b){
+
+  return a*b;
+
+ }
+
+ 
+
+ public int divide (int a, int b){
+
+  return a/b;
+
+ }
+
+}
